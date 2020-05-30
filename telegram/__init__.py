f4e8956c2040a557368989978613c8e7fc238212
Alexey Sokolov <juvarulven@gmail.com>
2020-05-30 15:48:48 +0300

    added method load_command_plugins in class Bot of module telegram

--- telegram/__init__.py
+++ telegram/__init__.py
@@ -9,6 +9,20 @@ class Bot(TeleBot):
         super().__init__(token)
 
     def message_handler(self, handler, commands=None, regexp=None, func=None, content_types=None, **kwargs):
+        """
+        Простая функция, которая до переопределения была декоратором.
+        Подробнее в документации на pyTelegramBotApi
+
+        :param handler: функция-обрабатчик сообщений из Телеграма
+        :param commands: список команд для реагирования
+        :param regexp: регулярное выражение
+        :param func: место под lambda-функцию. Если она вернет True,
+        обработчик будет вызван
+        :param content_types: список типов сообщений для реагирования.
+        по умолчанию ['text']
+        :param kwargs:
+        :return: функция, переданная как handler
+        """
         if content_types is None:
             content_types = ['text']
         handler_dict = self._build_handler_dict(handler,
@@ -19,3 +33,24 @@ class Bot(TeleBot):
                                                 **kwargs)
         self.add_message_handler(handler_dict)
         return handler
+
+    def load_command_plugins(self, plugins_list: list) -> None:
+        """
+        Загрузка плагинов-обработчиков комманд из телеграма через self.message_handler()
+
+        :param plugins_list: список словарей вида [{'handler': функция-обработчик, 'command': ['команда'...]}...]
+        :return: None
+        """
+        while plugins_list:
+            plugin = plugins_list.pop()
+            assert isinstance(plugin, dict), \
+                'список плагинов содержит не словари ' + repr(plugin)
+            assert len(plugin) == 2, \
+                'в словаре плагина неправильное число элементов ' + repr(plugin)
+            assert hasattr(plugin['handler'], '__call__'), \
+                'в словаре плагина по ключу "function" содержится не функция ' + repr(plugin)
+            assert isinstance(plugin['commands'], list), \
+                'в словаре плагина по ключу "commands" содержится не список ' + repr(plugin)
+            assert all(list(map(lambda type_of: isinstance(type_of, str), plugin['commands']))), \
+                'в списке "commands" словаря плагина содержатся не строки' + repr(plugin)
+            self.message_handler(plugin['handler'], commands=plugin['commands'])
