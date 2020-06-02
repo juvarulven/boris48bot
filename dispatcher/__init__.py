c0f1b93c38cb1abf5e1d119f654e3e3005637cb1
Alexey Sokolov <juvarulven@gmail.com>
2020-06-02 17:12:41 +0300

    added methods scheduler, task_handler, in module dispatcher

--- dispatcher/__init__.py
+++ dispatcher/__init__.py
@@ -1,5 +1,5 @@
 from threading import Thread
-import datetime
+from datetime import datetime, timedelta
 
 
 class Dispatcher(Thread):
@@ -9,10 +9,29 @@ class Dispatcher(Thread):
         self.scheduled_plugins = scheduled_plugins
         self.bot = bot
         self.running_flag = True
+        self.returns_stack = []
 
     def run(self):
+        if self.scheduled_plugins is None:
+            self.scheduled_plugins = []
         self.bot.load_command_plugins(self.plugins)
         self.bot.polling(none_stop=True)
         while self.running_flag:
-            # todo: сделать полезную функциональность
-            pass
+            self.scheduler()
+            while self.returns_stack:
+                task = self.returns_stack.pop()
+                self.task_handler(task)
+
+    def scheduler(self):
+        dt = datetime.utcnow()
+        for plugin in self.scheduled_plugins:
+            if 'next_time' not in plugin:
+                plugin['next_time'] = dt + timedelta(minutes=plugin['minutes'])
+            if plugin['next_time'] < dt:
+                plugin['next_time'] = dt + timedelta(minutes=plugin['minutes'])
+                self.returns_stack.append(plugin['handler']())
+
+    def task_handler(self, task):
+        if task['task'] == 'send text':
+            for telegram_id in task['id']:
+                self.bot.send_message(telegram_id, task['message'])
