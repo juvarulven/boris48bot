a634373a12243cc767add72032b9402331271b6a
Alexey Sokolov <juvarulven@gmail.com>
2020-06-03 18:29:54 +0300

    no comments

--- plugins/test_plugin.py
+++ plugins/test_plugin.py
@@ -1,3 +1,6 @@
+from bot_admin import ADMIN_ID
+
+
 def test_simple(bot, message):
     print('test_simple work')
     bot.send_message(message.from_user.id, 'passed!')
@@ -5,7 +8,7 @@ def test_simple(bot, message):
 
 def test_scheduled():
     print('sending scheduled test message')
-    return {'task': 'send_text', 'id': ['383489759'], 'message': 'scheduled test passed!'}
+    return {'task': 'send_text', 'id': [ADMIN_ID], 'message': 'scheduled test passed!'}
 
 
 __all__ = ['test_simple', 'test_scheduled']
