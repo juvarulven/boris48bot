de31fddde9bc2c9c9935e0a4b610fd9a18275409
Alexey Sokolov <juvarulven@gmail.com>
2020-06-15 18:23:24 +0300

    DROP TABLE

--- database/__init__.py
+++ database/__init__.py
@@ -1,9 +1,23 @@
 import bson
-import os
+import os.path
 
 
 class Database:
-    save_lock = False
+    file_lock = False
 
     def __init__(self, filename):
         self._filename = filename
+        self.db = None
+
+    def load(self, filename=None):
+        if filename is None:
+            filename = self._filename
+        if os.path.exists(filename):
+            with open(filename, 'rb') as db:
+                self.db = bson.loads(db.read())
+        else:
+            with open(filename, 'wb') as db:
+                db.write(bson.dumps({}))
+
+
+
