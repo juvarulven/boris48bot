9a7c06cff019feb87fba6972b59315f45775f8ca
Alexey Sokolov <juvarulven@gmail.com>
2020-06-16 13:38:55 +0300

    rework on database. All broken

--- database/__init__.py
+++ database/__init__.py
@@ -1,23 +1,43 @@
 import bson
 import os.path
+import time
 
 
 class Database:
-    file_lock = False
+    file_lock = {}
 
-    def __init__(self, filename):
-        self._filename = filename
-        self.db = None
+    def __init__(self, collection):
+        self._filename = collection + '.bson'
+        self.__class__.file_lock[self._filename] = False
+        self.documents = {}
 
-    def load(self, filename=None):
-        if filename is None:
-            filename = self._filename
-        if os.path.exists(filename):
-            with open(filename, 'rb') as db:
-                self.db = bson.loads(db.read())
+    def load(self):
+        if os.path.exists(self._filename):
+            with open(self._filename, 'rb') as table:
+                self.documents = bson.loads(table.read())
         else:
-            with open(filename, 'wb') as db:
-                db.write(bson.dumps({}))
+            self.save()
 
+    def save(self):
+        while self.__class__.file_lock[self._filename]:
+            time.sleep(0.01)
+        self.__class__.file_lock[self._filename] = True
+        with open(self._filename, 'wb') as table:
+            table.write(bson.dumps(self.documents))
+        self.__class__.file_lock[self._filename] = False
 
+    def add_document(self, name, *fields, **fields_with_content):
+        if name in self.documents:
+            document = self.documents[name]
+        else:
+            document = {}
+            self.documents[name] = document
+        for field in fields:
+            if field not in document:
+                document[field] = None
+        for field, content in fields_with_content:
+            document[field] = content
 
+    def get_document(self, name):
+        if name in self.documents:
+            return self.documents[name]
