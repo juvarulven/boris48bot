bd5cec813257367652eb67fa8a3c4adbc4319d63
Alexey Sokolov <juvarulven@gmail.com>
2020-06-18 21:35:47 +0300

    Added types in module vault_api, rework class Api. All broken

--- vault_api/__init__.py
+++ vault_api/__init__.py
@@ -1,68 +1,31 @@
+from .types import Stats, Comments, Diff, User, Node, Tag
 import log
 import requests
 
+TEST_URL = 'https://staging.vault48.org:3334/'
+MAIN_URL = 'https://vault48.org:3333/'
+
 
 class Api:
-    def __init__(self, url, port):
-        self._stats_url = '{}:{}/stats'.format(url, port)
-        self._flow_url = '{}:{}/node/diff'.format(url, port)
-        self._comments_url = '{}:{}/node/{}/comment'.format(url, port, '{}')
-        self._comments_count = 0
-        self._flow_timestamp = None
-        self._comments_nodes = {'boris': [696, '']}
-        self._timestamps_loaded = False
+    def __init__(self, testing=False):
+        if testing:
+            url = TEST_URL
+        else:
+            url = MAIN_URL
+        self._stats_url = url + 'stats'
+        self._node_url = url + 'node/{}'
+        self._diff_url = url + 'node/diff'
+        self._comments_url = url + 'node/{}/comment'
 
-    def _get_stats(self):
+    def get_stats(self):
         try:
-            return get_json(self._stats_url)
+            return Stats(get_json(self._stats_url))
         except Exception as error:
-            error_message = 'vault_plugin: Ошибка при попытке получить stats Убежища: ' + str(error)
+            error_message = 'vault_api: Ошибка при попытке получить stats Убежища: ' + str(error)
             log.log(error_message)
 
-    def _update_comments_count_and_timestamps(self):
-        result_timestamps = {}
-        json = self._get_stats()
-        if json:
-            timestamps = json['timestamps']
-            flow_current_timestamp = timestamps['flow_last_post']
-            if self._flow_timestamp is None or self._flow_timestamp < flow_current_timestamp:
-                result_timestamps['flow'] = flow_current_timestamp
-            boris_current_timestamp = timestamps['boris_last_comment']
-            boris_timestamp = self._comments_nodes['boris'][1]
-            if not boris_timestamp or boris_timestamp < boris_current_timestamp:
-                result_timestamps['boris'] = boris_current_timestamp
-            comments_count = json['comments']['total']
-            if self._comments_count < comments_count:
-                for key, value in self._comments_nodes.items():
-                    if key == 'boris':
-                        continue
-                    node_id = value[0]
-                    content = self._get_comments(node_id, 1)
-                    result_timestamps[key] = content['comments']['0']['created_at']
-            return comments_count, result_timestamps
-        else:
-            return 0, {}
-
-    def _get_updates(self):
-        pass
-
-    def get_comments_count_and_timestamps(self):
-        comments_count, timestamps = self._update_comments_count_and_timestamps()
-        if comments_count:
-            yield 'comments_count', comments_count
-            for name, timestamp in timestamps.items():
-                yield name, timestamp
-
-    def set_comments_count_and_timestamps(self, **kwargs):
-        for key, value in kwargs.items():
-            if key == 'comments_count':
-                self._comments_count = value
-            elif key == 'flow':
-                self._flow_timestamp = value
-            else:
-                self._comments_nodes[key] = value
-
-    def _get_diff(self, start=None, end=None, with_heroes=False, with_updated=False, with_recent=False, with_valid=False):
+    def get_diff(self, start=None, end=None, with_heroes=False,
+                 with_updated=False, with_recent=False, with_valid=False):
         params = {'start': start,
                   'end': end,
                   'with_heroes': with_heroes,
@@ -70,18 +33,25 @@ class Api:
                   'with_recent': with_recent,
                   'with_valid': with_valid}
         try:
-            return get_json(self._flow_url, params=params)
+            return Diff(get_json(self._diff_url, params=params))
         except Exception as error:
-            error_message = 'vault_plugin: Ошибка при попытке получить diff Убежища: ' + str(error)
+            error_message = 'vault_api: Ошибка при попытке получить diff Убежища: ' + str(error)
             log.log(error_message)
 
-    def _get_comments(self, node, take, skip=0):
+    def get_comments(self, node, take, skip=0):
         params = {'take': take,
                   'skip': skip}
         try:
-            return get_json(self._comments_url.format(node), params=params)
+            return Comments(get_json(self._comments_url.format(node), params=params))
         except Exception as error:
-            error_message = 'vault_plugin: Ошибка при попытке получить comments Убежища: ' + str(error)
+            error_message = 'vault_api: Ошибка при попытке получить comments Убежища: ' + str(error)
+            log.log(error_message)
+
+    def get_node(self, node):
+        try:
+            return Node(get_json(self._node_url.format(node)))
+        except Exception as error:
+            error_message = 'vault_api: Ошибка при попытке получить node Убежища: ' + str(error)
             log.log(error_message)
 
 
--- vault_api/types.py
+++ vault_api/types.py
@@ -1,8 +1,34 @@
+class Stats:
+    def __init__(self, dictionary):
+        self.users_total = dictionary['users']['total']
+        self.users_alive = dictionary['users']['alive']
+        self.nodes_images = dictionary['nodes']['images']
+        self.nodes_audios = dictionary['nodes']['audios']
+        self.nodes_videos = dictionary['nodes']['videos']
+        self.nodes_texts = dictionary['nodes']['texts']
+        self.nodes_total = dictionary['nodes']['total']
+        self.comments_total = dictionary['comments']['total']
+        self.files_count = dictionary['files']['count']
+        self.files_size = dictionary['files']['size']
+        self.timestamps_boris = dictionary['timestamps']['boris_last_comment']
+        self.timestamps_flow = dictionary['timestamps']['flow_last_post']
+
+
 class Comments:
     def __init__(self, dictionary):
-        self.comments = dictionary['comments']
+        self.comments = [Comment(comment) for comment in dictionary['comments']]
         self.order = dictionary['order']
         self.comment_count = dictionary['comment_count']
+
+
+class Diff:
+    def __init__(self, dictionary):
+        self.before = [DiffPost(post) for post in dictionary['before']]
+        self.after = [DiffPost(post) for post in dictionary['after']]
+        self.recent = [DiffPost(post) for post in dictionary['recent']]
+        self.heroes = [Hero(hero) for hero in dictionary['heroes']]
+        # self.updated = dictionary['updated']
+        # self.valid = dictionary['valid']
 
 
 class Comment:
@@ -13,6 +39,8 @@ class Comment:
         self.created_at = dictionary['created_at']
         self.updated_at = dictionary['updated_at']
         self.deleted_at = dictionary['deleted_at']
+        self.files = [File(file) for file in dictionary['files']]
+        self.user = User(dictionary['user'])
 
 
 class File:
@@ -30,3 +58,79 @@ class File:
         self.metadata = dictionary['metadata']
         self.created_at = dictionary['created_at']
         self.updated_at = dictionary['updated_at']
+
+
+class BasicUser:
+    def __init__(self, dictionary):
+        self.id = dictionary['id']
+        self.photo = File(dictionary['photo'])
+        self.username = dictionary['username']
+
+
+class User(BasicUser):
+    def __init__(self, dictionary):
+        super().__init__(dictionary)
+        self.email = dictionary['email']
+        self.role = dictionary['role']
+        self.fullname = dictionary['fullname']
+        self.description = dictionary['description']
+        self.is_activated = dictionary['dictionary']
+        self.last_seen = dictionary['last_seen']
+        self.last_seen_messages = dictionary['last_seen_messages']
+        self.created_at = dictionary['created_at']
+        self.updated_at = dictionary['updated_at']
+
+
+class BasicPost:
+    def __init__(self, dictionary):
+        self.id = dictionary['id']
+        self.title = dictionary['title']
+        self.type = dictionary['type']
+        self.created_at = dictionary['created_at']
+        self.commented_at = dictionary['commented_at']
+        self.thumbnail = dictionary['thumbnail']
+        self.description = dictionary['description']
+
+
+class DiffPost(BasicPost):
+    def __init__(self, dictionary):
+        super().__init__(dictionary)
+        self.user = User(dictionary['user'])
+
+
+class Node(BasicPost):
+    def __init__(self, dictionary):
+        dictionary = dictionary['node']
+        super().__init__(dictionary)
+        self.blocks = dictionary['blocks']
+        self.files_order = dictionary['files_order']
+        self.is_public = dictionary['is_public']
+        self.is_promoted = dictionary['is_promoted']
+        self.is_heroic = dictionary['is_heroic']
+        self.updated_at = dictionary['updated_at']
+        self.deleted_at = dictionary['deleted_at']
+        self.tags = [Tag(tag) for tag in dictionary['tags']]
+        self.files = [File(file) for file in dictionary['files']]
+        self.user = BasicUser(dictionary['user'])
+        self.cover = dictionary['cover']
+        self.is_liked = dictionary['is_liked']
+        self.like_count = dictionary['like_count']
+
+
+class Tag:
+    def __init__(self, dictionary):
+        self.id = dictionary['id']
+        self.title = dictionary['title']
+        self.data = dictionary['data']
+        self.created_at = dictionary['created_at']
+        self.updated_at = dictionary['updated_at']
+
+
+class Hero:
+    def __init__(self, dictionary):
+        self.id = dictionary['id']
+        self.thumbnail = dictionary['thumbnail']
+        self.title = dictionary['title']
+
+
+__all__ = ['Stats', 'Comments', 'Diff', 'User', 'Node', 'Tag']
