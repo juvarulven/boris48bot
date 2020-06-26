1d7a9089df95489e47a8b87cc1625acba09214e1
Alexey Sokolov <juvarulven@gmail.com>
2020-06-27 00:51:36 +0300

    docstrings and comments.

--- global_variables.py
+++ global_variables.py
@@ -6,5 +6,7 @@ RUNNING_FLAG = GlobalObject(True)
 # RUNNING_FLAG = _RUNNING_FLAG.value
 # но не прокатило, не фортануло: RUNNING_FLAG не импортируется. Приходится обращаться к значению через геттер-сеттер
 TELEGRAM_BOT = GlobalObject(None)
+# А этот парень нужен для того, чтобы вернуть экземпляр класса Bot для взаимодействия с ним
+# Тоже жуткий костыль
 
 __all__ = ['RUNNING_FLAG', 'TELEGRAM_BOT']
