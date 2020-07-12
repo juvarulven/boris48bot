8409bb291f57a4a95a47f8410073bad66f738809
Alexey Sokolov <juvarulven@gmail.com>
2020-07-12 19:02:51 +0300

    bugfixes

--- plugins/who_plugin.py
+++ plugins/who_plugin.py
@@ -6,14 +6,18 @@ from global_variables import TELEGRAM_BOT
 
 def who(message):
     answer = "Вот кого я знаю:\n"
+    row_template = '{} {} {} ({}) {} {}\n'
     users = TELEGRAM_BOT.value.get_users()
     for user_id, user_info in users.items():
-        answer += 'робот:   ' if user_info['is_bot'] else 'человек: '
-        answer += str(user_info['access_level']) + ' '
-        answer += user_id + ' '
-        answer += user_info['first_name'] + ' ('
-        answer += user_info['username'] + ') '
-        answer += user_info['last_name'] + '\n'
+        is_bot = 'робот:   ' if user_info['is_bot'] else 'человек: '
+        access_level = user_info['access_level']
+        first_name = user_info['first_name']
+        first_name = first_name if first_name else '...'
+        username = user_info['username']
+        username = username if username else '...'
+        last_name = user_info['last_name']
+        last_name = last_name if last_name else '...'
+        answer += row_template.format(user_id, is_bot, first_name, username, last_name, access_level)
     TELEGRAM_BOT.value.send_message(message.from_user.id, answer)
 
 
