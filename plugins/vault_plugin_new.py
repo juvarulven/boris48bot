4184553d006885a82c43dec84ff0a0c350f653fe
Alexey Sokolov <juvarulven@gmail.com>
2020-07-24 13:23:21 +0300

    started refactor vault_plugin

---
+++ plugins/vault_plugin_new.py
@@ -0,0 +1,33 @@
+class Main:
+    """
+    Главный класс бота
+    """
+    def __init__(self):
+        self._db = DB()
+        self._vault = Vault()
+        self._tg = Telegram()
+
+
+class DB:
+    """
+    Класс для взаимодействия с БД
+    """
+    def __init__(self):
+        pass
+
+
+class Vault:
+    """
+    Класс для общения с Убежищем
+    """
+    def __init__(self):
+        pass
+
+
+class Telegram:
+    """
+    Класс для взаимодействия с телеграммом
+    """
+    def __init__(self):
+        pass
+
