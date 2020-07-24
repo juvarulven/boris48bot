8fad45586ad36a0759ac89f39146271fe58cec16
Alexey Sokolov <juvarulven@gmail.com>
2020-07-25 00:41:26 +0300

    continued refactor

--- plugins/vault_plugin_new.py
+++ plugins/vault_plugin_new.py
@@ -17,7 +17,8 @@ class Main:
         self._init_db()
 
     def _init_db(self):
-        pass
+        if not self._db.first_load():
+
 
     def sub(self):
         pass
@@ -40,6 +41,7 @@ class DB:
         self._boris_timestamp = None
         self._boris_subscribers = []
         self._comments: Dict[str, Dict[str, Union[str, List[int]]]] = {}
+        # { 'node_id': {'title': 'название', 'timestamp': 'таймстамп', 'subscribers': [телеграм_id...]}... }
 
     def first_load(self) -> bool:
         """
@@ -61,6 +63,20 @@ class DB:
                 self._comments[document_name] = document
         return True
 
+    def _update_db(self, target):
+        if target == 'flow':
+            temp_document = { 'timestamp': self._flow_timestamp,
+                              'subscribers': self._flow_subscribers.copy() }
+        elif target == 'boris':
+            temp_document = { 'timestamp': self._boris_timestamp,
+                              'subscribers': self._boris_subscribers.copy() }
+        else:
+            temp_document = { 'title':       self._comments[target]['title'],
+                              'timestamp':   self._comments[target]['timestamp'],
+                              'subscribers': self._comments[target]['subscribers'].copy() }
+        self._db.update_document(target, fields_with_content=temp_document)
+        self._db.save_and_update()
+
     def get_timestamp(self, target: Union[int, str]) -> Optional[str]:
         """
         Возвращает таймстамп
@@ -75,6 +91,110 @@ class DB:
             target = str(target)
             if target.isdigit() and target in self._comments:
                 return self._comments[target]['timestamp']
+
+    def set_timestamp(self, target: Union[int, str], timestamp: str) -> None:
+        """
+        Устанавливает таймстамп и сохраняет в базу
+        :param target: 'flow', 'boris' или id ноды
+        :param timestamp: таймстамп
+        :return:
+        """
+        if target == 'flow':
+            self._flow_timestamp = timestamp
+            self._update_db('flow')
+        elif target == 'boris':
+            self._boris_timestamp = timestamp
+            self._update_db('boris')
+        else:
+            target = str(target)
+            if target.isdigit():
+                if target in self._comments:
+                    self._comments[target]["timestamp"] = timestamp
+                    self._update_db(target)
+
+    def get_subscribers(self, target: Union[str, int]) -> List[int]:
+        """
+        Возвращает список подписчиков
+        :param target: 'flow', 'boris' или id ноды Убежища
+        :return: список id телеграмма подписчиков
+        """
+        if target == 'flow':
+            return self._flow_subscribers.copy()
+        elif target == 'boris':
+            return self._boris_subscribers.copy()
+        else:
+            target = str(target)
+            if target in self._comments:
+                return self._comments[target]['subscribers'].copy()
+            return []
+
+
+    def add_subscriber(self, target: Union[str, int], telegram_id: int) -> bool:
+        """
+        Добавляет id телеграмма в список подписчиков и сохраняет в базу
+        :param target: 'flow', 'boris' или id ноды
+        :param telegram_id: id подписчика
+        :return: True если id подписчика не было в списке, иначе False
+        """
+        successfully = False
+        if target == 'flow':
+            if telegram_id not in self._flow_subscribers:
+                self._flow_subscribers.append(telegram_id)
+                self._update_db('flow')
+                successfully = True
+        elif target == 'boris':
+            if telegram_id not in self._boris_subscribers:
+                self._boris_subscribers.append(telegram_id)
+                self._update_db('boris')
+                successfully = True
+        else:
+            target = str(target)
+            if target.isdigit() and target not in self._comments[target]['subscribers']:
+                self._comments[target]['subscribers'].append(telegram_id)
+                self._update_db(target)
+                successfully = True
+        return successfully
+
+    def remove_subscriber(self, target: Union[str, int], telegram_id: int) -> bool:
+        """
+        Удаляет id телеграмма из списка подписчиков и сохраняет в базу
+        :param target: 'flow', 'boris' или id ноды
+        :param telegram_id: id подписчика
+        :return: True, если id телеграмма был в списке, иначе False
+        """
+        successfully = False
+        if target == 'flow':
+            if telegram_id in self._flow_subscribers:
+                self._flow_subscribers.remove(telegram_id)
+                self._update_db('flow')
+                successfully = True
+        elif target == 'boris':
+            if telegram_id in self._boris_subscribers:
+                self._boris_subscribers.remove(telegram_id)
+                self._update_db('boris')
+                successfully = True
+        else:
+            target = str(target)
+            if target.isdigit() and target in self._comments[target]['subscribers']:
+                self._comments[target]['subscribers'].remove(telegram_id)
+                self._update_db(target)
+                successfully = True
+        return successfully
+
+    def add_comments(self, node_id: Union[str, int], title: str, timestamp: str) -> None:
+        """
+        Добавляет ноды с комментариями и сохраняет в базу
+        :param node_id: id ноды
+        :param title: заголовок ноды
+        :param timestamp: таймстамп последнего комментария ноды
+        :return:
+        """
+        node_id = str(node_id)
+        if node_id not in self._comments:
+            temp_document = {'title': title, 'timestamp': timestamp, 'subscribers': []}
+            self._comments[node_id] = temp_document
+            self._update_db(node_id)
+
 
 
 class Vault:
